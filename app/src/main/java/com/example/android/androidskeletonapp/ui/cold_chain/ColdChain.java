b9eeab93b560dda9d14b919ff5b356bded6d21e4
Filip Larsen <Filip.larsen@live.no>
2021-10-06 20:14:43 +0200

    Add events from Json

--- app/src/main/java/com/example/android/androidskeletonapp/ui/cold_chain/ColdChain.java
+++ app/src/main/java/com/example/android/androidskeletonapp/ui/cold_chain/ColdChain.java
@@ -3,7 +3,6 @@ package com.example.android.androidskeletonapp.ui.cold_chain;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,23 +13,17 @@ import androidx.appcompat.widget.Toolbar;
 import com.example.android.androidskeletonapp.R;
 import com.example.android.androidskeletonapp.data.Sdk;
 
-import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.arch.call.D2Progress;
-import org.hisp.dhis.android.core.category.CategoryOptionComboCollectionRepository;
-import org.hisp.dhis.android.core.dataelement.DataElement;
 import org.hisp.dhis.android.core.enrollment.Enrollment;
-import org.hisp.dhis.android.core.enrollment.EnrollmentStatus;
 import org.hisp.dhis.android.core.event.Event;
-import org.hisp.dhis.android.core.event.EventCollectionRepository;
 import org.hisp.dhis.android.core.event.EventCreateProjection;
 import org.hisp.dhis.android.core.event.EventStatus;
 import org.hisp.dhis.android.core.maintenance.D2Error;
-import org.hisp.dhis.android.core.program.ProgramStage;
-import org.hisp.dhis.android.core.program.ProgramStageCollectionRepository;
+import org.hisp.dhis.android.core.program.ProgramStageDataElement;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValue;
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValueCollectionRepository;
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValueObjectRepository;
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityInstanceCreateProjection;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -42,14 +35,13 @@ import java.io.Writer;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
-import io.reactivex.Completable;
 import io.reactivex.Observable;
-import io.reactivex.Single;
 
 public class ColdChain extends AppCompatActivity {
 
+
+    private Object JSONArray;
 
     public static Intent getIntent(Context context){
         return new Intent(context, ColdChain.class);
@@ -75,9 +67,6 @@ public class ColdChain extends AppCompatActivity {
         System.out.println("download data : " + downloadData);
         Observable<D2Progress> downloadEvents = downloadSingleEvents();
         System.out.println("download events : " + downloadEvents);
-
-        String[] TeiUids = new String[] {"JroBkjBFKr6" ,"wJPMhboEUiw" ,"FGsKhDsTBEU" ,"Mk3wwXMD8Rm","uC5WM4DrtPN","t2YszIriCWS"};
-        String eventExample = " TrackedEntityDataValue{id=6, event=sLQQrsScOgZ, created=Tue Jun 15 21:30:02 GMT 2021, lastUpdated=Tue Jun 15 21:30:02 GMT 2021, dataElement=zEouq1KNiZO, storedBy=null, value=6.6, providedElsewhere=false}";
 
         //Denne iden funker - jeg får events på FGsKhDsTBEU
         String testTeiUid = "FGsKhDsTBEU";
@@ -111,62 +100,82 @@ public class ColdChain extends AppCompatActivity {
         createdval.setText("Created: " + getTeiCreatedValue(testTeiUid));
 
         addEvent("g5oklCs7xIg","SDuMzcGLh8i","aecqgkE5quA", "iMDPax84iAN");
-
+        JSONArray jsonData = getJsonArray();
+        for (int i = 0; i < jsonData.length(); i++) {
+            try {
+                System.out.println(jsonData.get(i));
+                JSONObject c = jsonData.getJSONObject(i);
+                String temp = c.getString("temperature");
+                String date = c.getString("Date");
+                System.out.println(temp);
+                System.out.println(date);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-
-
-    //setMetode -- set på vent ;)
+    // Creats a new event on this date foreach element in json file, fetches it's metadata dataElementUid and sets it value to temperature.
     public boolean addEvent (String enrollmentID, String programUid , String programStageId, String ouUid) {
         String defaultOptionCombo = Sdk.d2().categoryModule().categoryOptionCombos()
                 .byDisplayName().eq("default").one().blockingGet().uid();
         try {
-            String eventUid = Sdk.d2().eventModule().events().blockingAdd(
-                    EventCreateProjection.builder()
-                            .enrollment(enrollmentID)
-                            .program(programUid)
-                            .programStage(programStageId)
-                            .organisationUnit(ouUid)
-                            .attributeOptionCombo(defaultOptionCombo)
-                            .build()
-            );
+            JSONArray jsonData = getJsonArray();
+            for (int i = 0; i < jsonData.length(); i++) {
+                try {
+                    System.out.println(jsonData.get(i));
+                    JSONObject c = jsonData.getJSONObject(i);
+                    String temp = c.getString("temperature");
+                    String date = c.getString("Date");
+                    System.out.println(temp);
+                    System.out.println(date);
 
-            System.out.println("Ny Event UID :::::  " + eventUid);
-            Sdk.d2().eventModule().events().uid(eventUid).setEventDate(new Date());
-            Sdk.d2().enrollmentModule().enrollments().uid("g5oklCs7xIg").setEnrollmentDate(new Date());
-            Sdk.d2().eventModule().events().uid(eventUid).setCompletedDate(new Date());
+                    //makes a new event and returns it eventUID
+                    String eventUid = Sdk.d2().eventModule().events().blockingAdd(
+                            EventCreateProjection.builder()
+                                    .enrollment(enrollmentID)
+                                    .program(programUid)
+                                    .programStage(programStageId)
+                                    .organisationUnit(ouUid)
+                                    .attributeOptionCombo(defaultOptionCombo)
+                                    .build()
+                    );
 
-            Sdk.d2().enrollmentModule().enrollments().uid("g5oklCs7xIg").setStatus(EnrollmentStatus.ACTIVE);
+                    //sets the created-, enrollment- and event-date to this date.
+                    System.out.println("Ny Event UID :::::  " + eventUid);
+                    Sdk.d2().eventModule().events().uid(eventUid).setEventDate(new Date(date));
+                    Sdk.d2().enrollmentModule().enrollments().uid("g5oklCs7xIg").setEnrollmentDate(new Date(date));
+                    Sdk.d2().eventModule().events().uid(eventUid).setCompletedDate(new Date(date));
 
-            List<Event> attributes = Sdk.d2().eventModule()
-                    .events()
-                    .byUid().eq(eventUid)
-                    .withTrackedEntityDataValues()
-                    .blockingGet();
+                    //Get's the event's dataElement UID and set's a value to the event
+                    List<ProgramStageDataElement> programDataElem = Sdk.d2().programModule().programStageDataElements().blockingGet();
+                    for (ProgramStageDataElement elem: programDataElem
+                    ) {
+                        Sdk.d2().trackedEntityModule().trackedEntityDataValues().value(eventUid, elem.dataElement().uid()).blockingSet(temp);
+                    }
 
-            TrackedEntityDataValue addTempToEvent = TrackedEntityDataValue.builder().event(eventUid).value("69").created(new Date()).build();
+                    //set eventStatus and upload
+                    Sdk.d2().eventModule().events().uid(eventUid).setStatus(EventStatus.COMPLETED);
+                    Sdk.d2().eventModule().events().upload();
 
-            for (Event at : attributes) {
-                at.trackedEntityDataValues().add(addTempToEvent);
-                System.out.println("Event  : " + at);
-                System.out.println("AT ::: " + at.trackedEntityDataValues().add(addTempToEvent));
-                System.out.println(at.trackedEntityDataValues());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-
-            List<ProgramStage> programStages = Sdk.d2().programModule().programStages().blockingGet();
-
-
-            for ( ProgramStage p : programStages) {
-                System.out.println("Innenfor -- : " + p.reportDateToUse() );
-            }
-
-            Sdk.d2().eventModule().events().uid(eventUid).setStatus(EventStatus.COMPLETED);
-            Sdk.d2().eventModule().events().upload();
             return true;
         } catch (D2Error d2Error) {
             d2Error.printStackTrace();
             return false;
         }
+    }
+
+    public JSONArray getJsonArray(){
+        try {
+            return new JSONArray(getJson());
+        }catch (JSONException | IOException e ){
+            e.printStackTrace();
+        }
+        return null;
     }
 
     //Metoden fyller inn textview med json-data og viser en toast ved onClick.
@@ -253,12 +262,12 @@ public class ColdChain extends AppCompatActivity {
         return Sdk.d2().trackedEntityModule().trackedEntityInstances().blockingGetUids();
     }
 
-
+    // Downloads tei's
     private Observable<D2Progress> downloadTrackedEntityInstances() {
         return Sdk.d2().trackedEntityModule().trackedEntityInstanceDownloader()
                 .limit(10).limitByOrgunit(false).limitByProgram(false).download();
     }
-
+    // Downloads events
     private Observable<D2Progress> downloadSingleEvents() {
         return Sdk.d2().eventModule().eventDownloader()
                 .limit(10).limitByOrgunit(false).limitByProgram(false).download();
